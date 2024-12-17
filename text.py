a94d9a7bf3b08cee892eb8f931a7a028c7b4cf16
Polina Tokunova <66551692+polsha24@users.noreply.github.com>
2024-12-17 23:02:12 +0300

    add text.py
    it contains all text messages

---
+++ text.py
@@ -0,0 +1,40 @@
+GREETING = """🎄 Welcome to the <b>CINE NEW YEARS BOT</b>! 🎬
+
+Are you ready to ring in the New Year with the perfect movie? Whether you’re looking for heartwarming holiday classics, magical adventures, or side-splitting comedies, this bot is here to help you find just the right film to match your mood and celebration! 
+
+What can you do with this bot?
+- 🌟 Get personalized movie recommendations tailored to your preferences.
+- 🎥 Explore a variety of genres, including family-friendly films, romantic comedies, and festive favorites.
+- 💎 Discover hidden gems and iconic classics that capture the spirit of the New Year.
+- 🔄 Receive suggestions based on your past favorites and ratings.
+- 📝 Save your top picks to come back to later!
+
+Kick off the festivities with friends and family, and let us help you make your New Year’s celebration unforgettable!🌈✨ 
+
+💡 Click <b>"functions info"</b> to get to know about the features of our bot more precisely👀
+
+Happy Watching! ❄️🎄📽️"""
+
+FUNCTIONS = """Functionalities:
+
+☃️<b>Choose a Year Period</b>☃️: 
+
+Select a specific year or range of years to receive movie recommendations from that time. Perfect for nostalgic picks or discovering classics from the past!
+
+☃️<b>Random Button</b>☃️: 
+
+Feeling spontaneous? Click the "✨random✨" button to get a surprise film suggestion! Enjoy the excitement of discovering something new without bias.
+
+With these features, you're set to find the perfect film to celebrate the New Year! 🎉📽️"""
+
+FIRST_CAT = "1900-1949"
+SECOND_CAT = "1950-1979"
+THIRD_CAT = "1980-1999"
+FOURTH_CAT = "2000-2009"
+FIFTH_CAT = "2010-2024"
+RANDOM_CAT = "✨random✨"
+FUNCTION_BUTTON = "functions info"
+
+SEE_MORE = "Do you wanna see more films?"
+SEE_MORE_BUTTON = "See more!☃️"
+NO_MORE_FILMS = "There's no more films left 😔"
